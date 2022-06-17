6e83cc7d65f88145a7bf3defc6f01008a215f3ae
Vagner <vavarb@yahoo.com>
2022-06-17 00:42:18 -0300

    Renamed file

--- connection_arbitrage.py
+++ connection_arbitrage.py
@@ -1,5 +1,5 @@
 
-from vavabot_arbitrage_1_2_3 import Deribit, CredentialsSaved
+from vavabot_arbitrage_1_2_2 import Deribit, CredentialsSaved
 import time
 from lists import list_monitor_log
 import threading
